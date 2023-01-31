e254f876de7c686b81f0b1617e52d7a480086f46
Julien <benedetjulien@gmail.com>
2023-01-31 14:26:13 +0100

    Changing style when the right number is guessed, css manipulation with DOM

--- script.js
+++ script.js
@@ -16,10 +16,15 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  //No input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number ⛔️';
+    //Player guess the number
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Youhou ! 🎉';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    //Number is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high 📈';
@@ -29,6 +34,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You loose the game 😥';
       document.querySelector('.score').textContent = 0;
     }
+    //Number is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low 📉';
